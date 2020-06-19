cd8dc4cf12a0ced926e7bc54d7a0e23ab18d06f1
JanDonnermayer <jan@donnermayer.de>
2020-06-19 17:15:59 +0200

    Add sample app

---
+++ app/Results.SampleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using static Results.Result;
+
+
+namespace Results.SampleApp
+{
+    class Program
+    {
+        static Task Main(string[] args) =>
+            GetGreetingAsync()
+                .HandleAsync(
+                    dataHandler: Console.WriteLine,
+                    errorHandler: Console.Error.WriteLine
+                );
+
+        private static async Task<IResult<string>> GetGreetingAsync()
+        {
+            await Task.Yield();
+
+            try
+            {
+                if (DateTime.Now.Millisecond % 2 == 1)
+                    throw new Exception("An error occurred :(");
+
+                return Ok("Hello World!");
+
+            }
+            catch (Exception ex)
+            {
+                return Error<string>(ex);
+            }
+        }
+    }
+}
--- src/Results/src/Primitives/Result.cs
+++ src/Results/src/Primitives/Result.cs
@@ -32,4 +32,5 @@ namespace Results
 
         public TData? Data { get; }
     }
+
 }
